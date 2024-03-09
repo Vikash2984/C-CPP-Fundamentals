844f1697037ea7bfa01a47461a9c9948dd85d82f
Vikash Kumar Pandey <112485773+Vikash2984@users.noreply.github.com>
2024-03-09 19:15:06 +0530

    Add files via upload

---
+++ C++/Practice.cpp
@@ -0,0 +1,17 @@
+//Format specifiers
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b ; 
+    char c ;
+    float d;
+    double e;
+
+     cin >> a >> b >> c >> d >> e;
+     printf("\n%d\n%ld\n%c\n%0.3f\n%0.9lf",*&a,*&b,*&c,*&d,*&e);
+
+    return 0;
+}
